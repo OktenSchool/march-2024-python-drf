6a3946934d82f7a4a0c98c72c7555db96b72113b
Vitalii Demchyshyn <demchyshyn.v87@gmail.com>
2024-09-18 21:53:32 +0200

    main

--- configs/settings.py
+++ configs/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # my_apps
-    'apps.cars',
 ]
 
 MIDDLEWARE = [
--- configs/urls.py
+++ configs/urls.py
@@ -17,5 +17,4 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('cars', include('apps.cars.urls'))
 ]
