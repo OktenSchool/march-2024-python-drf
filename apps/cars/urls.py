4cfd65bf93fb707d81de6bd1173ba15be711b151
Vitalii Demchyshyn <demchyshyn.v87@gmail.com>
2024-09-24 09:12:52 +0200

    lesson3

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CarsListCreateView, CarRetrieveUpdateDestroyView
+
+from .views import CarRetrieveUpdateDestroyView, CarsListView
+
 urlpatterns = [
-    path('', CarsListCreateView.as_view(), name='cars_list_create'),
+    path('', CarsListView.as_view(), name='cars_list'),
     path('/<int:pk>', CarRetrieveUpdateDestroyView.as_view(), name='cars_retrieve_update_destroy'),
 ]
