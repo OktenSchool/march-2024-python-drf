56bb82781761bd4d97710b45fde61a949db65650
Vitalii Demchyshyn <demchyshyn.v87@gmail.com>
2024-10-01 19:13:41 +0200

    lesson7-hw

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import UsersListCreateView,UserMeView
+from .views import UsersListCreateView,UserMeView, UserBanView,UserUnBanView,UserToAdminView, AdminToUserView
 
 urlpatterns = [
     path('', UsersListCreateView.as_view(), name='users_list_create'),
     path('/me', UserMeView.as_view(), name='user_me'),
+    path('/<int:pk>/ban', UserBanView.as_view(), name='user_ban' ),
+    path('/<int:pk>/unban', UserUnBanView.as_view(), name='user_unban' ),
+    path('/<int:pk>/to_admin', UserToAdminView.as_view(), name='user_to_admin' ),
+    path('/<int:pk>/to_user', AdminToUserView.as_view(), name='admin_to_user' ),
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -13,6 +13,7 @@ class UsersListCreateView(ListCreateAPIView):
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
 
+
 class UserMeView(GenericAPIView):
     serializer_class = UserSerializer
     queryset = UserModel.objects.all()
@@ -21,4 +22,65 @@ class UserMeView(GenericAPIView):
     def get(self, *args, **kwargs):
         user = self.request.user
         serializer = UserSerializer(user)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+class UserBanView(GenericAPIView):
+    queryset = UserModel.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(id=self.request.user.id)
+
+    def patch(self, *args, **kwargs):
+        user = self.get_object()
+        if user.is_active:
+            user.is_active = False
+            user.save()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+class UserUnBanView(GenericAPIView):
+    queryset = UserModel.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(id=self.request.user.id)
+
+    def patch(self, *args, **kwargs):
+        user = self.get_object()
+        if not user.is_active:
+            user.is_active = True
+            user.save()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+class UserToAdminView(GenericAPIView):
+    queryset = UserModel.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(id=self.request.user.id)
+
+    def patch(self, *args, **kwargs):
+        user = self.get_object()
+        if not user.is_staff:
+            user.is_staff = True
+            user.save()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+class AdminToUserView(GenericAPIView):
+    queryset = UserModel.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(id=self.request.user.id)
+
+    def patch(self, *args, **kwargs):
+        user = self.get_object()
+        if user.is_staff:
+            user.is_staff = False
+            user.save()
+        serializer = UserSerializer(user)
+
         return Response(serializer.data, status.HTTP_200_OK)
